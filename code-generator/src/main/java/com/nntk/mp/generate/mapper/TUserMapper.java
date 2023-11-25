01ccbd438a091d82a9dd185d046ddd22d5d4d1b8
huanghaoming <1585685035@qq.com>
2023-11-25 19:57:16 +0800

    提交

---
+++ code-generator/src/main/java/com/nntk/mp/generate/mapper/TUserMapper.java
@@ -0,0 +1,16 @@
+package com.nntk.mp.generate.mapper;
+
+import com.nntk.mp.generate.entity.TUser;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author nntk
+ * @since 2023-01-28
+ */
+public interface TUserMapper extends BaseMapper<TUser> {
+
+}
